58fb05fea42171fde172157ce253e749a0360fea
Elliott Minns <elliott.minns@pm.me>
2025-07-22 12:28:27 -0500

    feat: Remove otp on signup

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -20,7 +20,7 @@ export const auth = betterAuth({
     username(),
     emailOTP({
       disableSignUp: true,
-      sendVerificationOnSignUp: true, // Send verification emails on signup
+      sendVerificationOnSignUp: false, // Send verification emails on signup
       async sendVerificationOTP({ email, otp }) {
         console.log('Sending OTP email to:', email, 'OTP:', otp);
 
